75df1dc3c8aa37b631635e1209833db533eb04d8
Ashutosh Gupta <ashutoshgupta2737@gmail.com>
2025-12-23 00:03:22 +0530

    completed register page frontend

--- components/auth/register-form.tsx
+++ components/auth/register-form.tsx
@@ -18,7 +18,7 @@ import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { FormError } from "@/components/form-error";
 import { FormSuccess } from "@/components/form-success";
-import { login } from "@/actions/login";
+import { register } from "@/actions/register";
 
 export const RegisterForm = () => {
   const [isPending, startTransition] = useTransition();
@@ -39,7 +39,7 @@ export const RegisterForm = () => {
     setSuccess(undefined);
 
     startTransition(() => {
-      login(values).then((data) => {
+      register(values).then((data) => {
         setError(data.error);
         setSuccess(data.success);
       });
--- schemas/index.ts
+++ schemas/index.ts
@@ -1,14 +1,14 @@
 import * as z from "zod";
 
 export const LoginSchema = z.object({
-  email: z.email({ message: "Invalid email" }),
+  email: z.email({ message: "Valid email required" }),
   password: z.string().min(1, {
     message: "Password is required",
   }),
 });
 
 export const RegisterSchema = z.object({
-  email: z.email({ message: "Invalid email" }),
+  email: z.email({ message: "Valid email required" }),
   password: z.string().min(6, {
     message: "Minimum 6 characters required",
   }),
